bee344731117ef39a105b31eab0605e26c10c26b
Binyam Mulat Abegaz <bini.blue.1996@gmail.com>
2025-04-12 15:22:46 +0300

    Implement issue management functionality with CRUD operations and associated models

--- api/app/router/endpoints/chat_router.py
+++ api/app/router/endpoints/chat_router.py
@@ -2,15 +2,15 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from app.usecase.chat_service import ChatService, get_chat_service
 
 chatRouter = APIRouter()
-chatSession = None
+CHATSESSION = None
 
 #end point that initiate the router 
 @chatRouter.post("/chat/new")
 async def initiate_chat(
     chatService: ChatService = Depends(get_chat_service)
 ):
-    global chatSession
-    chatSession = chatService.new_chat()
+    global CHATSESSION
+    CHATSESSION = chatService.new_chat()
 
     return {"message": "Chat session initiated."}
 
@@ -19,7 +19,7 @@ async def respond(
     prompt: str,
     chatService: ChatService = Depends(get_chat_service)
 ):
-    response = chatService.get_response(chat_session=chatSession, prompt=prompt)
+    response = chatService.get_response(chat_session=CHATSESSION, prompt=prompt)
 
     return {"message": "Response received for chat.", "response": response}
 
